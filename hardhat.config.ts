7e5ed981a13317e811d90dcf02a5372110e35d0f
Chan <add7348@outlook.com>
2024-04-26 12:40:14 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,6 +39,14 @@ const config: HardhatUserConfig = {
     arbitrum: {
       url: "https://arb-mainnet.g.alchemy.com/v2/3xJE5XydRW9R6svaRlbiXeyhVEm6yFFQ" || "",
       accounts: [vars.get("TEST1_KEY"), vars.get("TEST2_KEY")],
+    },
+    optimism: {
+      url: "https://opt-mainnet.g.alchemy.com/v2/C0rnovoXZQ6eo9Egfvw1sTfkdLGsvouh" || "",
+      accounts: [vars.get("TEST1_KEY"), vars.get("TEST2_KEY")],
+    },
+    base: {
+      url: "https://base-mainnet.g.alchemy.com/v2/TGwFWGfUH96rmhISEJYNY1o1kTvqYasp" || "",
+      accounts: [vars.get("TEST1_KEY"), vars.get("TEST2_KEY")],
     }
   },
   sourcify: {
@@ -47,7 +55,9 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: {
       arbitrumSepolia: 'IQWAAF48I9BVSMJA8BIVEGM8Z55DZXTBSD',
-      sepolia: 'VK61M4154HUEBDRPEUZXF3MW2J7GSK9WJW'
+      sepolia: 'VK61M4154HUEBDRPEUZXF3MW2J7GSK9WJW',
+      optimism: '79WNIY53FCZY116W61ZN8DDTE1THBBVKN7',
+      base: 'TJY2CHMTQMI1HF4WVQJES2CDVF4K733CUK',
     }
   }
 };
